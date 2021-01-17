e177f786add1f855e1180a807f965482bcf288af
Nayan <nkaul0515@icloud.com>
2021-01-17 13:57:41 -0500

    Virus Game Houses

--- sketch.js
+++ sketch.js
@@ -1,9 +1,25 @@
+const Bodies=Matter.Bodies; 
+const Engine=Matter.Engine;
+const World=Matter.World; 
+
+var house1, house2, house3, house4;
+var virus;
+var engine;
+var world;
+
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  house1 = createSprite(100, 0, 200, 200);
+  house2 = createSprite(700, 0, 200, 200);
+  house3 = createSprite(100, 350, 200, 200);
+  house4 = createSprite(700, 350, 200, 200);
+
+  engine = Engine.create();
+  world = engine.world;
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0,0,0);  
   drawSprites();
 }
